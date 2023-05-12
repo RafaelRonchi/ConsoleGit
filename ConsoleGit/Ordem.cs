a4c5d5272f785ea88b804f0812ee0d509e0c0f50
Rafael <rafael.e01@aluno.ifsc.edu.br>
2023-05-12 17:04:39 -0300

    Ordem crescente Console

--- ConsoleGit/Ordem.cs
+++ ConsoleGit/Ordem.cs
@@ -4,11 +4,38 @@
     {
         static void Main(string[] args)
         {
-            int[] listaInformada = new int[10];
-            Console.WriteLine("Digite 10 numeros: ");
-            for(int i = 0; i < listaInformada.Length; i++)
+            int[] listaUsuario = new int[10];
+            int[] listaCopia = new int[10];
+
+            Console.WriteLine("Digite 10 números inteiros: ");
+            for (int i = 0; i < listaUsuario.Length; i++)
             {
-                listaInformada[i] = int.Parse(Console.ReadLine());
+                listaUsuario[i] = int.Parse(Console.ReadLine());
+                listaCopia[i] = listaUsuario[i];
+            }
+
+            for (int i = 0; i < listaUsuario.Length; i++)
+            {
+                for (int j = 0; j < listaUsuario.Length - 1; j++) 
+                {
+                    if (listaUsuario[i] < listaUsuario[j])
+                    {
+                        int parametroNum = listaUsuario[i];
+                        listaUsuario[i] = listaUsuario[j];
+                        listaUsuario[j] = parametroNum;
+                    }
+                }
+            }
+            Console.WriteLine("\nLista informada: ");
+            for (int i = 0; i < listaCopia.Length; i++)
+            {
+                Console.Write(listaCopia[i] + " ");
+
+            }
+
+                Console.WriteLine("\nArrumado em ordem crescente: ");
+            for (int i = 0; i < listaUsuario.Length ; i++) {
+                Console.Write(listaUsuario[i] + " ");   
             }
 
         }
