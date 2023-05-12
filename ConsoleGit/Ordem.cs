19616a078cb29d40bed24431e0d78d50d73d8b12
Rafael <rafael.e01@aluno.ifsc.edu.br>
2023-05-12 16:27:26 -0300

    Jogo da forca console

---
+++ ConsoleGit/Ordem.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleGit
+{
+    internal class Ordem
+    {
+        static void Main(string[] args)
+        {
+            int[] listaInformada = new int[10];
+            Console.WriteLine("Digite 10 numeros: ");
+            for(int i = 0; i < listaInformada.Length; i++)
+            {
+                listaInformada[i] = int.Parse(Console.ReadLine());
+            }
+
+        }
+    }
+}
---
+++ forca/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace forca
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] palavras = new string[] { "abacaxi", "cenoura", "melao" };
+                        
+            Random randNum = new Random();
+            string sorteado = palavras[randNum.Next(0, 3)];
+            int tentativa = 0;
+            char letra;
+            bool forValida = false;
+
+            char[] palavraFatia = new char[sorteado.Length];
+            
+            for(int i = 0;  i < sorteado.Length; i++)
+            {
+                palavraFatia[i] = '_';
+            }
+            while (tentativa < 6)
+            {
+                Console.WriteLine("Vidas: " + tentativa);
+                for(int i = 0;i < palavraFatia.Length; i++)
+                {
+                    Console.Write(palavraFatia[i]);
+                }
+
+                forValida = false;
+                Console.WriteLine("\n Digite sua letra: ");
+                letra = char.Parse(Console.ReadLine());
+
+                for(int i = 0; i < sorteado.Length; i++)
+                {
+                    if (sorteado[i] == letra)
+                    {
+                        Console.WriteLine("Letra correta");
+                        forValida = true;
+
+                        palavraFatia[i] = letra; break;
+                    }
+                }
+                if (forValida != true)
+                {
+                    Console.WriteLine("Menos uma vida: ");
+                    tentativa++;
+                }
+            }
+            Console.WriteLine(sorteado);
+            Console.ReadKey();
+
+        }
+    }
+}
