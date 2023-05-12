5ea52091236e9a643536f6fd4485dfebb0a9da50
Rafael <rafael.e01@aluno.ifsc.edu.br>
2023-05-12 13:38:54 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleGit/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleGit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
