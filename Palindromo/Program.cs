8d969f855b754e9f30fec7b9fb85ed77b095ce80
Rafael <rafael.e01@aluno.ifsc.edu.br>
2023-05-12 16:41:12 -0300

    Palíndromo Console

---
+++ Palindromo/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Palindromo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string palavra = " ";
+            Console.WriteLine("Digite a palavra: ");
+            palavra = Console.ReadLine();
+
+           
+
+            char[] caracteres = palavra.ToCharArray();
+            Array.Reverse(caracteres);
+
+
+            int resultado = palavra.Length;
+            int total = 0;
+
+            for (int i = 0; i < caracteres.Length; i++)
+            {
+                if (palavra[i] == caracteres[i])
+                {
+                    total++;
+                }
+            }
+
+            if(total == resultado)
+            {
+                Console.WriteLine("A string " + palavra + " é um palindromo");
+            }
+            else
+            {
+                Console.WriteLine("A string " + palavra + " não é um palíndromo");
+
+            }
+
+        }
+    }
+}
